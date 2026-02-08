e9a12cf84893c4a1eb9a3d25ab81a2acfd669411
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-08 21:43:55 +0900

    Add healthcheck endpoint for Render debug

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def healthcheck(request):
+    return HttpResponse("Django is alive on Render")
 
 urlpatterns = [
+    path("health/", healthcheck),   # ← 追加（最重要）
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("", include("users.urls")),
