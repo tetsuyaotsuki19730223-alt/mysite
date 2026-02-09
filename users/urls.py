304cea9e2ff2164e6e03bc5a15bede1fd4ef3970
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-09 19:46:14 +0900

    Fix stripe_webhook import and add subscribe endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import stripe_webhook
+from .views import stripe_webhook, subscribe
 
 urlpatterns = [
     path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
+    path("subscribe/", subscribe, name="subscribe"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,33 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.shortcuts import redirect
+
 
 @csrf_exempt
 def stripe_webhook(request):
     return HttpResponse("ok")
+
+
+def subscribe(request):
+    try:
+        import stripe
+    except ImportError:
+        return HttpResponse("Stripe not installed", status=500)
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    session = stripe.checkout.Session.create(
+        mode="subscription",
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price": settings.STRIPE_PRICE_ID,
+                "quantity": 1,
+            }
+        ],
+        success_url="http://mysite-admin.onrender.com/success/",
+        cancel_url="http://mysite-admin.onrender.com/cancel/",
+    )
+
+    return redirect(session.url)
