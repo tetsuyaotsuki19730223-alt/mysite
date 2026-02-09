3f239382c614eaa6f6327ae01beb0bbe299d4f0b
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-09 19:10:18 +0900

    Enable stripe webhook endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import create_checkout_session
+from .views import stripe_webhook
 
 urlpatterns = [
-    path("subscribe/", create_checkout_session, name="subscribe"),
+    path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
 ]
