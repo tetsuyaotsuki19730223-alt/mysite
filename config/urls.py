4a3da89c2063b9282dbf21df301a29eaa0399e61
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-09 14:33:17 +0900

    Fix users URL routing

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def health(request):
-    return HttpResponse("OK")
 
 urlpatterns = [
-    path("health/", health),
     path("admin/", admin.site.urls),
     path("", include("users.urls")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import create_checkout_session, stripe_webhook
+from django.http import HttpResponse
 
-app_name = "users"
+def test(request):
+    return HttpResponse("users urls OK")
 
 urlpatterns = [
-    path("subscribe/", create_checkout_session, name="subscribe"),
-    path("subscription/success/", subscription_success, name="subscription_success"),
-    path("subscription/cancel/", subscription_cancel, name="subscription_cancel"),
-    path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
-    path("billing/portal/", customer_portal, name="customer_portal"),
+    path("subscribe/", test),
 ]
