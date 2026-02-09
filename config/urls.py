15bb951671bc5bee1cf1e62e0f2d4d28d649fa6a
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-09 10:17:25 +0900

    Restore admin url

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
-def healthcheck(request):
-    return HttpResponse("Django is alive on Render")
+from django.urls import path
 
 urlpatterns = [
-    #path("health/", healthcheck),   # ← 追加（最重要）
     path("admin/", admin.site.urls),
-    #path("", include("blog.urls")),
-    #path("", include("users.urls")),
 ]
