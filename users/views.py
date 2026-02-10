b43b52f72c39b7e92a569468467c63e79940ac57
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-10 16:30:24 +0900

    debug subscribe view

--- users/views.py
+++ users/views.py
@@ -1,36 +1,20 @@
-from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import redirect
-
-
-def get_stripe():
-    if not settings.STRIPE_ENABLED:
-        return None
-
-    try:
-        import stripe
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        return stripe
-    except ImportError:
-        return None
-
+from django.conf import settings
+import traceback
 
 def subscribe(request):
-    stripe = get_stripe()
-    if stripe is None:
-        return HttpResponse("Stripe is not configured", status=503)
-
-    session = stripe.checkout.Session.create(
-        mode="subscription",
-        payment_method_types=["card"],
-        line_items=[
-            {
-                "price": settings.STRIPE_PRICE_ID,
-                "quantity": 1,
-            }
-        ],
-        success_url=request.build_absolute_uri("/subscribe/success/"),
-        cancel_url=request.build_absolute_uri("/subscribe/cancel/"),
-    )
-
-    return redirect(session.url)
+    try:
+        return HttpResponse(
+            f"""
+            STRIPE_ENABLED={settings.STRIPE_ENABLED}
+            PRICE_ID={settings.STRIPE_PRICE_ID}
+            SECRET_SET={bool(settings.STRIPE_SECRET_KEY)}
+            """,
+            content_type="text/plain"
+        )
+    except Exception as e:
+        return HttpResponse(
+            traceback.format_exc(),
+            status=500,
+            content_type="text/plain"
+        )
