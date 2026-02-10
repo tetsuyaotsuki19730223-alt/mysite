6634d9adfb6370560837ae83e0e389994a3a44f7
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-10 17:57:01 +0900

    Fix users urls: remove missing stripe_webhook import

--- users/views.py
+++ users/views.py
@@ -1,12 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
+import stripe
 from django.conf import settings
 
 def subscribe(request):
-    return HttpResponse(
-        f"""
-    SUBSCRIBE VIEW OK
-    STRIPE_SECRET_SET={bool(settings.STRIPE_SECRET_KEY)}
-    STRIPE_PRICE_ID='{settings.STRIPE_PRICE_ID}'
-    STRIPE_ENABLED={settings.STRIPE_ENABLED}
-    """
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    session = stripe.checkout.Session.create(
+        mode="subscription",
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price": settings.STRIPE_PRICE_ID,
+                "quantity": 1,
+            }
+        ],
+        success_url="https://mysite-2-w9ja.onrender.com/success/",
+        cancel_url="https://mysite-2-w9ja.onrender.com/cancel/",
     )
+
+    return redirect(session.url)
