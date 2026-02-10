64055c3d8992067cfd6f7b77e4dba4ea506f9ba2
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-10 21:12:12 +0900

    Add stripe webhook endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import subscribe
+from .views import subscribe, stripe_webhook
 
 urlpatterns = [
     path("subscribe/", subscribe, name="subscribe"),
+    path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
 ]
