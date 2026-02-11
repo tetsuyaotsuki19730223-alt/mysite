730500a0abc7d88234bfa0d6aa18e40a798b631e
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-11 13:18:29 +0900

    Switch subscribe to Stripe Checkout safely

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import subscribe, stripe_webhook
+from django.http import HttpResponse
 
 urlpatterns = [
     path("subscribe/", subscribe),
     path("stripe/webhook/", stripe_webhook),
+    path("success/", lambda r: HttpResponse("SUCCESS")),
+    path("cancel/", lambda r: HttpResponse("CANCEL")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 import stripe
 
 
 def subscribe(request):
-    return HttpResponse("SUBSCRIBE OK")
+    # 念のためガード
+    if not settings.STRIPE_ENABLED:
+        return HttpResponse("Stripe disabled", status=503)
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    session = stripe.checkout.Session.create(
+        mode="subscription",
+        line_items=[
+            {
+                "price": settings.STRIPE_PRICE_ID,
+                "quantity": 1,
+            }
+        ],
+        success_url=request.build_absolute_uri("/success/"),
+        cancel_url=request.build_absolute_uri("/cancel/"),
+    )
+
+    # Stripe公式推奨：リダイレクト
+    return HttpResponseRedirect(session.url)
 
 
 @csrf_exempt
