3bf70c20c65ab551600277aadf18f3db7fd6402e
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-11 12:47:16 +0900

    Switch subscribe view to Stripe Checkout

--- users/views.py
+++ users/views.py
@@ -1,4 +1,18 @@
+from django.conf import settings
 from django.http import HttpResponse
+import stripe
 
 def subscribe(request):
-    return HttpResponse("SUBSCRIBE OK")
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    session = stripe.checkout.Session.create(
+        mode="subscription",
+        line_items=[{
+            "price": settings.STRIPE_PRICE_ID,
+            "quantity": 1,
+        }],
+        success_url=request.build_absolute_uri("/success/"),
+        cancel_url=request.build_absolute_uri("/cancel/"),
+    )
+
+    return HttpResponse(f"REDIRECT:{session.url}")
