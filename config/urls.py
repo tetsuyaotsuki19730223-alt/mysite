0692def9868127724769e61ff2821865b57f7fb1
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-13 13:28:50 +0900

    Add Django auth login URLs

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("users.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")), 
+    path("", include("users.urls")),
 ]
