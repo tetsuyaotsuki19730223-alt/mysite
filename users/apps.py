cdb1575d55105ea16aef95957e1281030cb856c5
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-14 17:42:29 +0900

    Auto create superuser

--- users/apps.py
+++ users/apps.py
@@ -1,9 +1,22 @@
 from django.apps import AppConfig
-
+import os
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
-    def ready(self):
-        from . import signals  # noqa
+def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if os.environ.get("CREATE_SUPERUSER") == "true":
+            username = "admin"
+            password = "admin123"
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email="admin@example.com",
+                    password=password
+                )
+                print("🔥 SUPERUSER CREATED")
