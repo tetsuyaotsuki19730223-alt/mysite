152a51dc398e0659d26f07608070c857cb5e79f8
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-02-18 11:23:49 +0900

    add blog level system

--- users/models.py
+++ users/models.py
@@ -2,21 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+
     LEVEL_CHOICES = [
-        ("beginner", "Beginner"),
-        ("intermediate", "Intermediate"),
+        ("free", "Free"),
+        ("basic", "Basic"),
+        ("pro", "Pro"),
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_subscribed = models.BooleanField(default=False)
-    current_price_id = models.CharField(max_length=200, blank=True, null=True)
-    stripe_customer_id = models.CharField(max_length=200, blank=True, null=True)
-
     blog_level = models.CharField(
         max_length=20,
         choices=LEVEL_CHOICES,
-        default="beginner"
+        default="free"
     )
-
-    def __str__(self):
-        return self.user.username
